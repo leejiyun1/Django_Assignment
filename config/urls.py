eb1b0e52d03e88b4c32e10a40d1a6bf69f5c8b3d
“leejiyun1” <poi20701556@gmail.com>
2025-04-30 13:51:11 +0900

    ✨ CBV
    CBV를 이용해서 수정함.

--- config/urls.py
+++ config/urls.py
@@ -19,13 +19,16 @@ def user_info(request, user_id):
 
 
 urlpatterns = [
-    path('todo/', todo_list, name='todo_list'),
+    path('todo/', todo_list, name='cbv_todo_list'),  # ❌ 이름이 잘못된 부분도 있음
     path('todo/create/', todo_create, name='todo_create'),
     path('todo/<int:todo_id>/', todo_info, name='todo_info'),
     path('todo/<int:todo_id>/update/', todo_update, name='todo_update'),
     path('todo/<int:todo_id>/delete/', todo_delete, name='todo_delete'),
+
+    path('cbv/', include('todo.urls')),  # ✅ CBV용 URL 추가
+
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/login/', user_views.login, name='login'),
-    path('accounts/signup/', user_views.sign_up, name='signup')
+    path('accounts/signup/', user_views.sign_up, name='signup'),
 ]
